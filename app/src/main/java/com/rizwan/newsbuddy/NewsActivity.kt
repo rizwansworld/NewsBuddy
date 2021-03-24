47b482461268707a4af38b036bb3a722fc786dc9
Rizwan Aboobacker <rizwan-old@Rizwans-MacBook-Air.local>
2021-03-24 20:51:01 +0400

    Project initial setup
    First setup of project

---
+++ app/src/main/java/com/rizwan/newsbuddy/NewsActivity.kt
@@ -0,0 +1,11 @@
+package com.rizwan.newsbuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
